05bad1cedbafcc89a92569e656d1d4c24d493bfe
AritraBanik08 <87798039+AritraBanik08@users.noreply.github.com>
2024-03-16 18:54:55 +0530

    Update index.nim

--- src/index.nim
+++ src/index.nim
@@ -2,20 +2,50 @@ import
   mike,
   segfaults,
   os,
+  httpclient,
+  json,
+  marshal,
   nimja/parser,
-  ./a3pkg/[models, mics]
+  dotenv,
+  ./a3pkg/[models, mics],
+  ./a3c/users
 
 proc index*(ctx: Context): string =
+  dotenv.load()
   var
     products: seq[Products]
     cookies = ctx.cookies
+    user: User
+    db = newDatabase()
 
     email = cookies.getOrDefault("email", "")
     password = cookies.getOrDefault("password", "")
 
-  if email == "":
-    echo "No cookie found."
-  else:
+    url = getEnv("DATABASE_URL")
+    auth_token = getEnv("AUTH_TOKEN")
+
+    client = newHttpClient()
+
+    body = """
+      "requests": [
+        { "type": "execute", "stmt": { "sql": "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(255) NOT NULL)" } },
+        { "type": "close" }
+      ]
+    """
+      # "requests": [
+      #   { "type": "execute", "stmt": { "sql": "SELECT * FROM users" } },
+      #   { "type": "close" }
+      # ]
+
+  ctx.addHeader("Authorization", "Bearer " & auth_token)
+  ctx.addHeader("Content-Type", "application/json")
+
+  # echo ctx.headers
+  var res = client.request(url, HttpPost, body, ctx.headers)
+  echo $$res
+
+  if email != "":
     products = micsGetProducts(email, password)
+    user = db.getUser(email, password)
 
   compileTemplateFile(getScriptDir() / "a3a" / "index.nimja")
