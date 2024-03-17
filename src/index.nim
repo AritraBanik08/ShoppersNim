4eb2ff9b93161896b7ac59240f2b6ba62e076681
AritraBanik08 <87798039+AritraBanik08@users.noreply.github.com>
2024-03-17 17:39:27 +0530

    Update index.nim

--- src/index.nim
+++ src/index.nim
@@ -2,16 +2,11 @@ import
   mike,
   segfaults,
   os,
-  httpclient,
-  json,
-  marshal,
   nimja/parser,
-  dotenv,
   ./a3pkg/[models, mics],
   ./a3c/users
 
 proc index*(ctx: Context): string =
-  dotenv.load()
   var
     products: seq[Products]
     cookies = ctx.cookies
@@ -20,29 +15,6 @@ proc index*(ctx: Context): string =
 
     email = cookies.getOrDefault("email", "")
     password = cookies.getOrDefault("password", "")
-
-    url = getEnv("DATABASE_URL")
-    auth_token = getEnv("AUTH_TOKEN")
-
-    client = newHttpClient()
-
-    body = """
-      "requests": [
-        { "type": "execute", "stmt": { "sql": "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(255) NOT NULL)" } },
-        { "type": "close" }
-      ]
-    """
-      # "requests": [
-      #   { "type": "execute", "stmt": { "sql": "SELECT * FROM users" } },
-      #   { "type": "close" }
-      # ]
-
-  ctx.addHeader("Authorization", "Bearer " & auth_token)
-  ctx.addHeader("Content-Type", "application/json")
-
-  # echo ctx.headers
-  var res = client.request(url, HttpPost, body, ctx.headers)
-  echo $$res
 
   if email != "":
     products = micsGetProducts(email, password)
